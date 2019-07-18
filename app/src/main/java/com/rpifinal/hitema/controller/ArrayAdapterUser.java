56663ef9f94b36d92d8f11a01ec4538589026bba
vince1396 <vincent.cotini96@gmail.com>
2019-07-18 11:30:39 +0200

    Propreté code

--- app/src/main/java/com/rpifinal/hitema/controller/ArrayAdapterUser.java
+++ app/src/main/java/com/rpifinal/hitema/controller/ArrayAdapterUser.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.rpifinal.hitema.R;
@@ -19,7 +18,6 @@ public class ArrayAdapterUser extends ArrayAdapter<User> {
 
     // =============================================================================================
     // ATTRIBUTS MEMBRES
-
     private Context context;
     private ArrayList<User> liste_user;
     private User user;
@@ -42,12 +40,11 @@ public class ArrayAdapterUser extends ArrayAdapter<User> {
     @NonNull
     public View getView (int position, @Nullable View convertView, @Nullable ViewGroup parent) {
 
-        LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         user = liste_user.get(position);
         @SuppressLint("ViewHolder") View vueCustom_User = inflater.inflate(R.layout.activity_array_adapter_user, parent,false);
 
-
-        ImageView imageListViewProfile =vueCustom_User.findViewById(R.id.list_user_view_picture);
+        ImageView imageListViewProfile = vueCustom_User.findViewById(R.id.list_user_view_picture);
         TextView textView_username = vueCustom_User.findViewById(R.id.tv_username);
         TextView textView_lvl = vueCustom_User.findViewById(R.id.tv_lvl);
         TextView textView_exp = vueCustom_User.findViewById(R.id.tv_exp);
@@ -56,7 +53,7 @@ public class ArrayAdapterUser extends ArrayAdapter<User> {
         String s_exp = exp.toString();
 
         Integer lvl = (user.getLvl());
-        String s_lvl= lvl.toString();
+        String s_lvl = lvl.toString();
 
         textView_username.setText(user.getUsername());
         textView_lvl.setText(s_lvl);
