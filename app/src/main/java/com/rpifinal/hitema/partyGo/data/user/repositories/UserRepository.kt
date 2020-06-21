48c9217c515168f69c541afb9a6b32e58f00c8ff
d_lapilus <d.lapilus@alphalives.com>
2020-06-22 00:10:39 +0200

    New UI v0.2.1 - Debug GetUser

--- app/src/main/java/com/rpifinal/hitema/partyGo/data/user/repositories/UserRepository.kt
+++ app/src/main/java/com/rpifinal/hitema/partyGo/data/user/repositories/UserRepository.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.rpifinal.hitema.partyGo.data.user.model.User
+import kotlinx.coroutines.awaitAll
 import oldFiles.api.UserHelper
 import javax.inject.Inject
 
@@ -14,22 +15,29 @@ import javax.inject.Inject
 class UserRepository @Inject constructor()
 {
     private val TAG = "UserRepository"
+    private val user: MutableLiveData<User> = MutableLiveData<User>()
 
     fun getUser(uid: String): LiveData<User> {
-        val user: MutableLiveData<User> = MutableLiveData<User>()
-        UserHelper.getUser(uid).addOnSuccessListener {
+        if(getInfoUser(uid)){
+            Log.d(TAG, "User before return : " + user.value.toString())
+            Log.d(TAG, user.toString())
+            return user
+        }
+        return user
+    }
+
+    fun getInfoUser( uid : String) : Boolean{
+        return UserHelper.getUser(uid).addOnSuccessListener {
             Log.d(TAG, uid)
             Log.d(TAG, it.toString())
             if(it.exists()) {
                 Log.d(TAG, "Document exists")
                 Log.d(TAG, it.data.toString())
                 user.value = it.toObject(User::class.java)
+                Log.d(TAG, user.value.toString())
             } else {
                 Log.d(TAG, "Document doesn't exist")
             }
-        }
-        Log.d(TAG, "User before return : " + user.value.toString())
-        Log.d(TAG, user.toString())
-        return user
+        }.isSuccessful()
     }
 }
