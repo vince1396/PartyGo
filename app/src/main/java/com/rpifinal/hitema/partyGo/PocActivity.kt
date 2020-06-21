cdcf0c9f20174624b7af536079089c01de1c2efe
d_lapilus <d.lapilus@alphalives.com>
2020-06-21 19:33:17 +0200

    New UI v0.2.1 - Ajout Fragment Dynamique

--- app/src/main/java/com/rpifinal/hitema/partyGo/PocActivity.kt
+++ app/src/main/java/com/rpifinal/hitema/partyGo/PocActivity.kt
@@ -1,6 +1,8 @@
 package com.rpifinal.hitema.partyGo
 
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.rpifinal.hitema.R
 import com.rpifinal.hitema.databinding.ActivityPocBinding
 import com.rpifinal.hitema.partyGo.data.user.viewModel.UserViewModel
@@ -15,5 +17,17 @@ class PocActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poc)
+        if(savedInstanceState == null)
+            showFragment()
+
     }
+
+    fun showFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentHolder, MainMenuFragment())
+            .commitAllowingStateLoss()
+        println("AddedFragment")
+    }
+
+
 }
